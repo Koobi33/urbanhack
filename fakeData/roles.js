8d48edaca447082ff15d46cf21abb3a6f8beb5a7
Сучков Дмитрий Вадимович <Suchkov.D.Vad@sberbank.ru>
2019-11-21 00:07:32 +0300

    init

---
+++ fakeData/roles.js
@@ -0,0 +1,29 @@
+import business from '../assets/images/business-card-cover.jpg';
+import integrator from '../assets/images/integrator-card-cover.jpeg';
+import goverment from '../assets/images/goverment-card-cover.jpg';
+import user from '../assets/images/user-card-cover.jpg';
+const roles = [
+  {
+    id: 0,
+    name: 'Бизнес',
+    image: business,
+  },
+  {
+    id: 1,
+    name: 'Интегратор',
+    image: integrator,
+  },
+  {
+    id: 2,
+    name: 'Правительство',
+    image: goverment,
+  },
+  {
+    id: 3,
+    name: 'Клиент',
+    image: user,
+  }
+  ]
+;
+
+export default roles;
