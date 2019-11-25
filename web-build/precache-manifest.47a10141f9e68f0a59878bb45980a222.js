918c06aca96f86ef5c481f0c28266c1a1482acf7
Сучков Дмитрий Вадимович <Suchkov.D.Vad@sberbank.ru>
2019-11-25 15:57:49 +0300

    builded web

---
+++ web-build/precache-manifest.47a10141f9e68f0a59878bb45980a222.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6fcad9effad1823ae55",
+    "url": "/static/js/app.140a69b8.chunk.js"
+  },
+  {
+    "revision": "440be4b4ed4ec37d369a",
+    "url": "/static/js/runtime~app.82cccf13.js"
+  },
+  {
+    "revision": "4ce8b6949aaa89d28304",
+    "url": "/static/js/2.e3454535.chunk.js"
+  },
+  {
+    "revision": "37acfdca148730251f75301a1504cae1",
+    "url": "/static/media/business-card-cover.37acfdca.jpg"
+  },
+  {
+    "revision": "822dd0e4e5bd2a59a6c4ce949e9cdd4b",
+    "url": "/static/media/integrator-card-cover.822dd0e4.jpeg"
+  },
+  {
+    "revision": "c5edb22db50324b66adf27538bbab559",
+    "url": "/static/media/government-card-cover.c5edb22d.jpg"
+  },
+  {
+    "revision": "2a39a161902debc7d8b90aad3edc2447",
+    "url": "/static/media/user-card-cover.2a39a161.jpg"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "282d21a694f8e7a8cca362ceb69d665e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "eb5e5b0c94a2e839d780339aa795ada9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf87d5074f4ecf87b46525ee8a1e496",
+    "url": "/static/js/app.140a69b8.chunk.js.gz"
+  },
+  {
+    "revision": "b460a9bb76b08d0e02560573c1220b3e",
+    "url": "/static/js/runtime~app.82cccf13.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "e7549ccaa8d6a1d315d2c17354400bd3",
+    "url": "/static/js/2.e3454535.chunk.js.gz"
+  }
+];
