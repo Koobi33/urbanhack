0e0b51fe16477ea45350b90d9678581384769836
Сучков Дмитрий Вадимович <Suchkov.D.Vad@sberbank.ru>
2019-12-02 08:44:26 +0300

    deploy

---
+++ web-build/precache-manifest.4b6e812df303f965aa29fa2eddbf79a9.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "64fab767ea2a24a7e774",
+    "url": "/static/js/app.9e656cd2.chunk.js"
+  },
+  {
+    "revision": "4bce1c964b3a20d00ca1",
+    "url": "/static/js/runtime~app.5125e157.js"
+  },
+  {
+    "revision": "497d1c1eebd87dfcf18f",
+    "url": "/static/js/2.1ac03528.chunk.js"
+  },
+  {
+    "revision": "dd39fa38bf38c2d7044631b60775d0ac",
+    "url": "/static/media/logo.dd39fa38.svg"
+  },
+  {
+    "revision": "1b851e8333aa5c366f0f6c310373562b",
+    "url": "/static/media/valuta.1b851e83.svg"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "dd451f3899240cc3e6736ed63465766f",
+    "url": "/static/media/1.dd451f38.svg"
+  },
+  {
+    "revision": "6f49e81b712221e063ec6e9e5ad777dd",
+    "url": "/static/media/2.6f49e81b.svg"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "a3ad22217962f95d236a9b2472a4f7ba",
+    "url": "/static/media/gif_irina.a3ad2221.gif"
+  },
+  {
+    "revision": "87eb15234d228aa217466cfee39a56ae",
+    "url": "/static/media/ezgif.com-optimize.87eb1523.gif"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "282d21a694f8e7a8cca362ceb69d665e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4787ff7129284abffb568d62e36f5dae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd3660b2d792a624a67e0b9433c7a89",
+    "url": "/static/js/runtime~app.5125e157.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "738b19f68ad0e16f8b87cc485dd08ea0",
+    "url": "/static/js/app.9e656cd2.chunk.js.gz"
+  },
+  {
+    "revision": "1e6f9564f424f0a19d21a57f391ec7a7",
+    "url": "/static/js/2.1ac03528.chunk.js.gz"
+  }
+];
